245c51102e1f79839ca142a4b19f63d30cc6a655
Saikat Sharma <saikatsharma4@gmail.com>
2022-01-02 19:27:17 +0600

    inti project

---
+++ Controllers/NameController.cs
@@ -0,0 +1,52 @@
+﻿using JWTAuthentication.Models;
+using JWTAuthentication.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWTAuthentication.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager _jwtAuthenticationManager;
+
+        public NameController(IJwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this._jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+
+        // GET: api/<NameController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Saikat", "Sharma" };
+        }
+
+        // GET api/<NameController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User user)
+        {
+            var token = _jwtAuthenticationManager.Authenticate(user.username, user.password);
+            if(token is null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+    }
+}
---
+++ Services/JwtAuthenticationManager.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.Services
+{
+    public class JwtAuthenticationManager : IJwtAuthenticationManager
+    {
+        private readonly IDictionary<string, string> users = new Dictionary<string, string>
+        {
+            {"test1", "password1" },
+            {"test2", "password2" },
+            {"test3", "password3" },
+        };
+        private readonly string key;
+        public JwtAuthenticationManager(string key)
+        {
+            this.key = key;
+        }
+
+        public string Authenticate(string username, string password)
+        {
+            if(!users.Any(usr => usr.Key == username && usr.Value == password))
+            {
+                return null;
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.ASCII.GetBytes(key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject  = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Expires = DateTime.Now.AddHours(1),
+                SigningCredentials = 
+                new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
